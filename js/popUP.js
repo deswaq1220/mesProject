b1cca8f4b49e5157a18ff33ba73bf8b7d26c178f
deswaq789 <deswaq890@naver.com>
2023-05-10 00:36:54 +0900

    팝업,엑셀저장

---
+++ js/popUP.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+
+  $("#contents").click(function() {
+
+      $("#popup").fadeIn();
+
+  });
+
+  $(".exit").click(function() {
+
+      $("#popup").fadeOut();
+
+  });
+
+});
